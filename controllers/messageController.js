3c6dc09351c0aed2e8cfc13402a7e4021398343b
JakeBrowning90 <jacobbrowning90@gmail.com>
2024-06-07 16:05:11 -0400

    Add basic functionality to Message CRUD operations

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -5,21 +5,38 @@ const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
 
 exports.message_create = asyncHandler(async (req, res, next) => {
-  res.json("Created message");
+  const message = new Message({
+    author: req.body.author,
+    recipient: req.body.recipient,
+    body: req.body.body,
+  });
+
+  await message.save();
+  res.json(message);
 });
 
 exports.message_read_all = asyncHandler(async (req, res, next) => {
-  res.json("Read all messages");
+  const allMessages = await Message.find().exec();
+  res.json(allMessages);
 });
 
 exports.message_read = asyncHandler(async (req, res, next) => {
-  res.json("Read message");
+  const message = await Message.findById(req.params.id).exec();
+  res.json(message);
 });
 
 exports.message_update = asyncHandler(async (req, res, next) => {
-  res.json("Updated message");
+  const message = new Message({
+    author: req.body.author,
+    recipient: req.body.recipient,
+    body: req.body.body,
+    _id: req.params.id,
+  });
+  await Message.findByIdAndUpdate(req.params.id, message);
+  res.json(message);
 });
 
 exports.message_delete = asyncHandler(async (req, res, next) => {
+  await Message.findByIdAndDelete(req.params.id);
   res.json("Deleted message");
 });
