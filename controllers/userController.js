bbb2ff8b971e731fdf8bcce6dbdeb34dc45512e7
JakeBrowning90 <jacobbrowning90@gmail.com>
2024-06-17 13:21:59 -0400

    Add validation to user update route

--- controllers/userController.js
+++ controllers/userController.js
@@ -46,8 +46,8 @@ exports.user_create = [
       await user.save();
       res.json(user);
     }
-  })
-]
+  }),
+];
 
 exports.user_read_all = asyncHandler(async (req, res, next) => {
   const search = new RegExp(`${req.query.name}`);
@@ -85,16 +85,50 @@ exports.user_log_in = asyncHandler(async (req, res, next) => {
   );
 });
 
-exports.user_update = asyncHandler(async (req, res, next) => {
-  const user = new User({
-    email: req.body.email,
-    password: req.body.password,
-    contacts: req.body.contacts,
-    _id: req.params.id,
-  });
-  await User.findByIdAndUpdate(req.params.id, user);
-  res.json(user);
-});
+exports.user_update = [
+  body("email")
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage("Email required")
+    .isEmail()
+    .withMessage("Invalid email address")
+    .isLength({ max: 30 })
+    .withMessage("Email must not exceed 30 characters")
+    .custom(async (value) => {
+      const existingUser = await User.findOne({ email: value });
+      if (existingUser) {
+        throw new Error("Email already in use.");
+      }
+    }),
+    body("password")
+      .trim()
+      .isLength({ min: 8, max: 20 })
+      .withMessage("Password must be between 8 and 20 characters"),
+    body("confirm_password")
+      .custom((value, { req }) => {
+        return value === req.body.password;
+      })
+      .withMessage("Typed passwords do not match"),
+
+    asyncHandler(async (req, res, next) => {
+      const errors = validationResult(req);
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+      const user = new User({
+        email: req.body.email,
+        password: hashedPassword,
+        contacts: req.body.contacts,
+        _id: req.params.id,
+      });
+      await User.findByIdAndUpdate(req.params.id, user);
+      res.json(user);
+      if (!errors.isEmpty()) {
+        res.json(errors.array());
+      } else {
+        await user.save();
+        res.json(user);
+      }
+    }),
+  ]
 
 exports.user_delete = asyncHandler(async (req, res, next) => {
   await User.findByIdAndDelete(req.params.id);
